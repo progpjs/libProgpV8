ac3f7f431e04257956513293a79db3fbd472ff71
johanpiquet <johan.piquet@gmail.com>
2024-02-27 12:38:34 +0100

    Add generated files

---
+++ generated.h
@@ -0,0 +1,9 @@
+#ifndef PROGP_STANDALONE
+
+
+void progpJsFunctionCaller_1(FCT_CALLBACK_PARAMS, const char* p0_val, size_t p0_size,int p1, double p2, const char* p3_buffer, size_t p3_size, const char* p4_val, size_t p4_size);
+
+void progpJsFunctionCaller_2(FCT_CALLBACK_PARAMS, const char* p0_val, size_t p0_size, const char* p1_val, size_t p1_size);
+
+
+#endif // PROGP_STANDALONE
