030131c2023a07f2070f90eca5022e74d2e8286f
johanpiquet <johan.piquet@gmail.com>
2024-03-04 15:49:55 +0100

    Improving file cache management

--- generated.h
+++ generated.h
@@ -1,9 +1,7 @@
 #ifndef PROGP_STANDALONE
 
 
-void progpJsFunctionCaller_1(FCT_CALLBACK_PARAMS, const char* p0_val, size_t p0_size,int p1, double p2, const char* p3_buffer, size_t p3_size, const char* p4_val, size_t p4_size);
-
-void progpJsFunctionCaller_2(FCT_CALLBACK_PARAMS, const char* p0_val, size_t p0_size, const char* p1_val, size_t p1_size);
+void progpJsFunctionCaller_1(FCT_CALLBACK_PARAMS, double p0, const char* p1_val, size_t p1_size);
 
 
 #endif // PROGP_STANDALONE
